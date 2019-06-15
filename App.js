a776aca04aa384d918c53f6869d57843637fc067
Henry Paulino <henrypl360@gmail.com>
2019-06-15 14:47:51 -0400

    get weather by city and coordinates

--- App.js
+++ App.js
@@ -1,29 +1,81 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, TextInput, Button } from 'react-native';
+// import RNLocation from 'react-native-location';
 
 type Props = {};
+
+const API_KEY = '55e371b73eb5f5b1da3c34e8d7513c79';
 export default class App extends Component<Props> {
+  state = {
+    temp: 0,
+    pressure: 0,
+    humidity: 0,
+    name: '',
+    city: '',
+  }
+
+  constructor() {
+    super();
+    this.geolocation = navigator.geolocation;
+  }
+
+  componentDidMount() {
+    this.geolocation.requestAuthorization();
+
+    this.getLocation();
+  }
+
+  getLocation = async () => {
+    this.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      console.log('setLocation', { latitude, longitude })
+      this.setState({ lat: latitude, long: longitude });
+      this.getWeather(latitude, longitude)
+    });
+  }
+
+  getWeather = async () => {
+    const url = 'https://api.openweathermap.org/data/2.5';
+    const { lat, long, city } = this.state;
+    const searchMethod = city ? `q=${city}` : `lat=${lat}&lon=${long}`;
+    try {
+      const res = await fetch(`${url}/weather?${searchMethod}&units=imperial&APPID=${API_KEY}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      });
+      const { main, name } = await res.json();
+      this.setWeather({ name, ...main});
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  setWeather = ({ temp, pressure, humidity, name }) => {
+    this.setState({ temp, pressure, humidity, name })
+  }
+
   render() {
+    const { temp, pressure, humidity, name, city } = this.state
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={{ width: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+          <TextInput
+            value={city}
+            onChangeText={text => this.setState({ city: text })}
+            style={styles.searchInput}
+            placeholder="Enter city"
+          />
+          <Button title="Search" onPress={this.getWeather} />
+        </View>
+        <View style={styles.weatherContainer}>
+          <Text style={styles.name}>{name}</Text>
+          <Text>{temp}</Text>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={{ marginRight: 10 }}>{pressure}</Text>
+            <Text>{humidity}</Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -32,9 +84,9 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingTop: 70,
+    paddingHorizontal: 16,
   },
   welcome: {
     fontSize: 20,
@@ -46,4 +98,23 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    letterSpacing: 0.6
+  },
+  weatherContainer: {
+
+  },
+  searchInput: {
+    height: 50,
+    width: '80%',
+    borderWidth: 1,
+    borderColor: 'rgb(240, 240, 240)',
+    paddingHorizontal: 10,
+    borderRadius: 6,
+  },
+  seearchBtn: {
+
+  }
 });
